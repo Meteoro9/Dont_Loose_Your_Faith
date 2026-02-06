a57146a6f63dfe63a23374b2104a609182d4b0fa
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-06 14:47:36 -0300

    Lista de tiempos localizable también!!!

--- Scripts/Utils/main_menu.gd
+++ Scripts/Utils/main_menu.gd
@@ -4,13 +4,22 @@ class_name MainMenu
 @onready var label = $Label
 var languages: Array[String] = ["en_US", "es_AR"]
 
+var your_records_string: Array[String] = ["Your records: \n", "Tus tiempos: \n"]
+var list_empty_string: Array[String] = ["(No records registered yet)", "(Aún no se registró ningún tiempo)"]
+
 func _ready() -> void:
-	var final_text = "Tus tiempos: \n"
+	update_text()
+
+func update_text():
+	var current_lang = TranslationServer.get_locale()
+	var current_index = languages.find(current_lang)
+	if current_index == -1: current_index = 0
+	var final_text = your_records_string[current_index]
 	
 	var record_list = GameData.records
 	
 	if record_list.is_empty():
-		final_text += "(Aún no se registró ningún tiempo)"
+		final_text += list_empty_string[current_index]
 	else:
 		# Duplicamos el array y lo reordenamos al revés, el último queda arriba
 		var splash_record_list = record_list.duplicate()
@@ -28,7 +37,6 @@ func _ready() -> void:
 	await get_tree().process_frame # Esperamos el primer frame
 	GlobalMusicManager.current_state = GlobalMusicManager.Scene_State.MENU
 
-
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/sample_level.tscn")
 
@@ -41,6 +49,8 @@ func on_language_pressed():
 	# Cambiamos al siguiente en la lista
 	var next_index = (current_index + 1) % languages.size()
 	change_language(languages[next_index])
+	
+	update_text()
 
 func change_language(lang: String) -> void:
 	TranslationServer.set_locale(lang)
