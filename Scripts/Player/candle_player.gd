1d56eba44434957772ab28a5e0b136aae043237f
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-08 16:23:55 -0300

    Si el jugador ganó, no se reciben inputs

--- Scripts/Player/candle_player.gd
+++ Scripts/Player/candle_player.gd
@@ -12,22 +12,23 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("arriba") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("izquierda", "derecha")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if not won:
+		# Handle jump.
+		if Input.is_action_just_pressed("arriba") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 		
-		# Ejecutamos mecánica principal
-		fire_animation.take_damage(1.5)
-		if fire_animation.current_flame <= 0:
-			fire_animation.kill()
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("izquierda", "derecha")
+		if direction:
+			velocity.x = direction * SPEED
+			
+			# Ejecutamos mecánica principal
+			fire_animation.take_damage(1.5)
+			if fire_animation.current_flame <= 0:
+				fire_animation.kill()
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 	move_and_slide()
