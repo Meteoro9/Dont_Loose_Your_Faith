4ef446b8cc5485066311b59924a44256cb2c0f9d
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-15 21:09:45 -0300

    Enemigo salta y tileset corregido

--- Scripts/Enemies/bad_slime.gd
+++ Scripts/Enemies/bad_slime.gd
@@ -3,6 +3,7 @@ class_name BadSlime
 
 enum State { ATTACKING, MOVING, FOLLOWING }
 
+const JUMP_VELOCITY = -400.0
 var _current_state : State = State.MOVING
 var damage := 4.0
 var speed := 500.0
@@ -12,11 +13,14 @@ var yendo := true
 var is_player_in_hitbox := false
 var player : CandlePlayer
 
+#region COMPORTAMIENTOS
 func _ready() -> void:
 	init_position_left = global_position.x
 	player = get_tree().get_first_node_in_group("player")
+	$AnimatedSprite2D.frame_changed.connect(_on_attack_started)
 
 func _process(delta: float) -> void:
+	if not is_on_floor(): velocity += get_gravity() * delta # Activamos gravedad
 	
 	_play_animation_and_motion(delta)
 	move_and_slide()
@@ -42,10 +46,12 @@ func _enemy_motion(_delta):
 	if _current_state == State.FOLLOWING:
 		var direction : float = sign(player.global_position.x - global_position.x)
 		velocity.x = direction * speed * _delta
-		
-		pass
-	# Añadir persecución al jugador
 
+func _on_attack_started():
+	if $AnimatedSprite2D.animation == "attack" and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+#region ÁREAS
 # Área dañado
 func _on_hurt_box_body_entered(body: Node2D) -> void:
 	if body is CandlePlayer:
