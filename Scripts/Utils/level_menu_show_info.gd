727de3ed717dd945ae53238312d7df4aca1bafbf
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-18 21:11:59 -0300

    Menú se centra cada vez que se consulta info

--- Scripts/Utils/level_menu_show_info.gd
+++ Scripts/Utils/level_menu_show_info.gd
@@ -7,10 +7,13 @@ class_name LevelShowInfo
 @export var stars_record_label : Label
 @export var level_info_array_ordered : Array[LevelSummaryMenu]
 var _current_index := 0
+@export var h_box_container : HBoxContainer
 
 func _ready() -> void:
 	await get_tree().create_timer(1.0).timeout
 	_show_info(level_info_array_ordered[0])
+	h_box_container.alignment = BoxContainer.ALIGNMENT_CENTER
+	h_box_container.global_position.y = h_box_container.global_position.y -20
 
 func _show_info(summary: LevelSummaryMenu) -> void:
 	var times_text := ""
@@ -28,17 +31,26 @@ func _show_info(summary: LevelSummaryMenu) -> void:
 	day_record_label.text = day_text
 	hour_record_label.text = hour_text
 	stars_record_label.text = stars_text
-	
 
+
+#region Buttons!!!
 func _on_button_next_pressed():
 	if (_current_index + 1) < level_info_array_ordered.size():
 		_current_index += 1
 		_show_info(level_info_array_ordered[_current_index])
+		h_box_container.alignment = BoxContainer.ALIGNMENT_CENTER
+		h_box_container.global_position.y = h_box_container.global_position.y -20
+
 
 func _on_button_previous_pressed():
 	if not (_current_index -1) < 0:
 		_current_index -= 1
 		_show_info(level_info_array_ordered[_current_index])
+		h_box_container.alignment = BoxContainer.ALIGNMENT_CENTER
+		h_box_container.global_position.y = h_box_container.global_position.y -20
 
 func _on_button_play_pressed():
 	LoadBar.fade_to_scene(level_info_array_ordered[_current_index].level_path)
+
+func _on_button_prev_menu_pressed():
+	pass
