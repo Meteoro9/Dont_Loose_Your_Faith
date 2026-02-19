29429106753b9d1d3cde46cbac8b2e08a5ac061b
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-19 20:53:05 -0300

    Al salir del mapa el jugador muere!
    De esta forma se aísla la única forma de mostrar el menú de perder

--- Scripts/Utils/win_area.gd
+++ Scripts/Utils/win_area.gd
@@ -2,15 +2,17 @@ extends Area2D
 class_name WinArea
 
 @export var win_scene : PackedScene
-@export var loose_scene : PackedScene
+#@export var loose_scene : PackedScene
 @export var is_win : bool
 @export var show_timer : CanvasLayer
 @export var level_id : int
 
 var timer : TimerRecord = null
+var fire: FireBehaviour = null
 
 func _ready() -> void:
 	timer = show_timer.get_node("Label")
+	fire = get_parent().get_node("Candle-Player/FireArea")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CandlePlayer:
@@ -30,6 +32,5 @@ func _on_body_entered(body: Node2D) -> void:
 			GameData.add_record(level_id, timer.current_time)
 			
 		elif not is_win: 
+			if fire: fire.kill()
 			GlobalMusicManager.current_state = GlobalMusicManager.Scene_State.LOOSE
-			var scene = loose_scene.instantiate()
-			get_tree().root.add_child(scene)
