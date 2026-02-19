dcf732fb3b05b8bed3b4bf09b657b14e3db3d929
Francisco MT Mazzone <tolal.mazzone@gmail.com>
2026-02-18 22:50:46 -0300

    Menús funcionales y compatibles

--- Scripts/Utils/level_menu_show_info.gd
+++ Scripts/Utils/level_menu_show_info.gd
@@ -5,33 +5,101 @@ class_name LevelShowInfo
 @export var day_record_label : Label
 @export var hour_record_label : Label
 @export var stars_record_label : Label
+
 @export var level_info_array_ordered : Array[LevelSummaryMenu]
 var _current_index := 0
 @export var h_box_container : HBoxContainer
+@export var animation : AnimationPlayer
+
+var languages: Array[String] = ["en_US", "es_AR", "pt_BR", "zh_CN", "ja_JP", "ru_RU"]
+var current_lang := "en_US"
+var list_empty_string: Array[String] = ["(No records registered yet)", 
+	"(Aún no se registró ningún tiempo)", "(Ainda não há tempos registrados)", 
+	"（尚未记录任何时间）", "（まだ記録がありません）", "(Пока нет записанных результатов)"]
+
 
 func _ready() -> void:
+	animation.play("init")
 	await get_tree().create_timer(1.0).timeout
 	_show_info(level_info_array_ordered[0])
 	h_box_container.alignment = BoxContainer.ALIGNMENT_CENTER
 	h_box_container.global_position.y = h_box_container.global_position.y -20
 
 func _show_info(summary: LevelSummaryMenu) -> void:
+	current_lang = TranslationServer.get_locale()
+	var current_lang_index = languages.find(current_lang)
+	if current_lang_index == -1: current_lang_index = 0
+	
 	var times_text := ""
 	var day_text := ""
 	var hour_text := ""
 	var stars_text := ""
 	
-	for entry in summary.processed_records:
-		times_text += "*" + entry["time"] + "\n"
-		day_text += entry["date"] + "\n"
-		hour_text += entry["hour"] + "\n"
-		stars_text += "★".repeat(entry["stars"]) + "☆".repeat(5 - entry["stars"]) + "\n"
-	
+	if summary.processed_records.is_empty():
+		times_text = list_empty_string[current_lang_index]
+	else:
+		for entry in summary.processed_records:
+			times_text += "*" + entry["time"] + "\n"
+			day_text += entry["date"] + "\n"
+			hour_text += entry["hour"] + "\n"
+			stars_text += "★".repeat(entry["stars"]) + "☆".repeat(5 - entry["stars"]) + "\n"
+		
 	times_record_label.text = times_text
 	day_record_label.text = day_text
 	hour_record_label.text = hour_text
 	stars_record_label.text = stars_text
 
+func show_level_info():
+	animation.play("appear")
+	await animation.animation_finished
+	animation.play("default")
+
+func hide_level_info():
+	animation.play("desappear")
+	await animation.animation_finished
+	visible = false
+	var animation_menu = get_parent().get_node("AnimationPlayer")
+	animation_menu.play("show_menu")
+
+""" Antigua implementación del lenguaje en menú
+	current_lang = TranslationServer.get_locale()
+	var current_index = languages.find(current_lang)
+	if current_index == -1: current_index = 0
+	
+	var raw_list = GameData.current_records
+	# Filtramos records del nivel actual
+	var level_list : Array[LevelRecord] = []
+	for rec in raw_list:
+		if rec.level_id == viewing_level_id:
+			level_list.append(rec)
+	
+	var text_times := ""
+	var text_dates := ""
+	var text_hours := ""
+	
+	if level_list.is_empty():
+		text_times = list_empty_string[current_index]
+		text_dates = ""
+		text_hours = ""
+	else:
+		# Duplicamos el array y lo reordenamos al revés, el último queda arriba
+		var splash_record_list = level_list.duplicate()
+		splash_record_list.reverse()
+		for rec in splash_record_list:
+			# Creamos un string formateado TIEMPO:
+			var time_formatted = "%.2f" % rec.time_record
+			# Lo añadimos a el string final
+			text_times += "* " + time_formatted + "\n"
+			
+			text_dates += rec.get_date_string() + "\n"
+			text_hours += rec.get_hour_string() + "\n"
+	
+	# Actualizamos label
+	label_times_record.text = text_times
+	label_day_record.text = text_dates
+	label_hour_record.text = text_hours
+	
+"""
 
 #region Buttons!!!
 func _on_button_next_pressed():
@@ -53,4 +121,4 @@ func _on_button_play_pressed():
 	LoadBar.fade_to_scene(level_info_array_ordered[_current_index].level_path)
 
 func _on_button_prev_menu_pressed():
-	pass
+	hide_level_info()
--- Scripts/Utils/main_menu.gd
+++ Scripts/Utils/main_menu.gd
@@ -1,66 +1,22 @@
 extends Control
 class_name MainMenu
 
-@onready var label_times_record = $LabelTimesRecord
-@onready var label_day_record = $LabelDayRecord
-@onready var label_hour_record = $LabelHourRecord
-var viewing_level_id := 0 # índice actual de records de nivel 
+@onready var level_info = $"Level Show Info"
+@onready var animation = $AnimationPlayer
 
 var languages: Array[String] = ["en_US", "es_AR", "pt_BR", "zh_CN", "ja_JP", "ru_RU"]
 var current_lang := "en_US"
 
-var list_empty_string: Array[String] = ["(No records registered yet)", 
-	"(Aún no se registró ningún tiempo)", "(Ainda não há tempos registrados)", 
-	"（尚未记录任何时间）", "（まだ記録がありません）", "(Пока нет записанных результатов)"]
-
 func _ready() -> void:
-	update_text()
-
-func update_text():
-	current_lang = TranslationServer.get_locale()
-	var current_index = languages.find(current_lang)
-	if current_index == -1: current_index = 0
-	
-	var raw_list = GameData.current_records
-	# Filtramos records del nivel actual
-	var level_list : Array[LevelRecord] = []
-	for rec in raw_list:
-		if rec.level_id == viewing_level_id:
-			level_list.append(rec)
-	
-	var text_times := ""
-	var text_dates := ""
-	var text_hours := ""
-	
-	if level_list.is_empty():
-		text_times = list_empty_string[current_index]
-		text_dates = ""
-		text_hours = ""
-	else:
-		# Duplicamos el array y lo reordenamos al revés, el último queda arriba
-		var splash_record_list = level_list.duplicate()
-		splash_record_list.reverse()
-		for rec in splash_record_list:
-			# Creamos un string formateado TIEMPO:
-			var time_formatted = "%.2f" % rec.time_record
-			# Lo añadimos a el string final
-			text_times += "* " + time_formatted + "\n"
-			
-			text_dates += rec.get_date_string() + "\n"
-			text_hours += rec.get_hour_string() + "\n"
-	
-	# Actualizamos label
-	label_times_record.text = text_times
-	label_day_record.text = text_dates
-	label_hour_record.text = text_hours
-	
+	level_info.visible = false
 	# Cambiar estado de la música:
 	await get_tree().process_frame # Esperamos el primer frame
 	GlobalMusicManager.current_state = GlobalMusicManager.Scene_State.MENU
 
 func _on_button_pressed() -> void:
-	LoadBar.fade_to_scene("res://Scenes/Levels/sample_level.tscn")
-	#get_tree().change_scene_to_file("res://Scenes/Levels/sample_level.tscn")
+	level_info.visible = true
+	level_info.show_level_info()
+	animation.play("hide_menu")
 
 func on_language_pressed():
 	# Buscamos el índice actual
@@ -71,8 +27,7 @@ func on_language_pressed():
 	# Cambiamos al siguiente en la lista
 	var next_index = (current_index + 1) % languages.size()
 	change_language(languages[next_index])
-	
-	update_text()
+
 
 func change_language(lang: String) -> void:
 	TranslationServer.set_locale(lang)
